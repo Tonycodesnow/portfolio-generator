559cfe29f60b3eb34e12d67833618944985cd9da
Ant <huertinez.family@outlook.com>
2021-09-28 12:26:28 -0700

    Export the Functions and Wrap Up

--- src/page-template.js
+++ src/page-template.js
@@ -4,7 +4,7 @@ const generateAbout = aboutText => {
         return '';
     }
 
-    return`
+    return `
         <section class="my-3" id="about">
             <h2 classs="text-dark bg-primary p-2 display-inline-block">About Me</h2>
             <P>${aboutText}</P>
@@ -13,39 +13,43 @@ const generateAbout = aboutText => {
 }
 
 const generateProjects = projectsArr => {
-    return`
+    return `
         <section class="my-3" id="portfolio">
         <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
         <div class="flex-row justify-space-between">
-        ${projectsArr.filter(({ feature }) => feature)
-            .map(({ name, decsription, languages, link }) => {
-                return`
-                <div class="col-12 mb-2 bg-dark text-light p3">
-                    <h3 class="portfolio-item-title text-light">${name}</h3>
-                    <h5 class="portfolio-languages">
-                     Built With:
-                     ${languages.join(', ')}
-                    </h5>
-                    <p>${decsription}</p>
-                    <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+        ${projectsArr
+            .filter(({ feature }) => feature)
+            .map(({ name, description, languages, link }) => {
+            return ` 
+                <div class ="col-12 mb-2 bg-dark text-light p3">
+                    <h3 class ="portfolio-item-title text-light">${name}</h3>
+                    <h5 class ="portfolio-languages">
+                        Built With:
+                        ${languages.map(language => language).join(',')}
+                    </h5> 
+                <p>${description}</p>
+                
+                <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
                 </div>
-            `;
+        `;
+        console.log(desc)
             })
             .join('')}
 
-        ${projectsArr.filter(({ feature }) => !feature)
+        ${projectsArr
+        .filter(({ feature }) => !feature)
         .map(({ name,  description, languages, link }) => {
-            return`
+            return ` 
             <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-                <h3 class="portfolio-item-title text-light">${name}</h3>
-                <h5 class="portfolio-languages">
-                 Built With:
-                 ${languages.join(', ')}
-                </h5>
-                <p>${description}</p>
-                <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2></i>View Project on GitHub</a>
-            </div>
-            `;
+            <h3 class="portfolio-item-title text-light">${name}</h3>
+            <h5 class="portfolio-languages">
+            Built With:
+                ${languages.join(', ')}
+            </h5>
+            <p>${description}</p>
+            <a href= "${link}" class="btn mt-auto"><i class="fab fa-github mr-2></i>View Project on GitHub</a>
+        </div>
+        `;
         })
         .join('')}
         </div>
@@ -81,9 +85,7 @@ module.exports = templateData => {
       <div class="container flex-row justify-space-between align-center py-3">
         <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
         <nav class="flex-row">
-          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
-            header.github
-          }">GitHub</a>
+          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">GitHub</a>
         </nav>
       </div>
     </header>
